11e07858995c808db245ba87428f1fd0a4427fb4
ArhamGarg <61930302+ArhamGarg@users.noreply.github.com>
2020-04-11 19:58:23 +0530

    Add files via upload

--- Box.js
+++ Box.js
@@ -11,9 +11,12 @@ class Box {
       World.add(world, this.body);
     }
     display(){
+      push();
+      translate(this.body.position.x, this.body.position.y);
       rectMode(CENTER);
       fill(255);
       rect(0,0, this.width, this.height);
+      pop();
     }
   };
   
--- sketch.js
+++ sketch.js
@@ -11,12 +11,12 @@ function setup(){
   engine = Engine.create();
   world = engine.world;
 
-  box1 = new Box(100,100,50,100);
-  box2 = new Box(200,100,50,100);
-  box3 = new Box(400,100,50,100);
-  box4 = new Box(500,100,50,100);
+  box1 = new Box(250,100,50,200);
+  box2 = new Box(300,100,50,100);
+  box3 = new Box(350,100,50,100);
+  box4 = new Box(400,100,50,200);
 
-  ground = new Ground(200,390,400,20);
+  ground = new Ground(200,390,800,20);
 }
 
 function draw(){
